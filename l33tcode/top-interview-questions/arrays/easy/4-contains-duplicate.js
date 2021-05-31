7fd37d4c0e5f14e5b65b085c1ca664781b7c35f4
Nick Papadakis <nickfin2014@gmail.com>
2021-05-31 12:10:45 -0700

    added solution and perf eval of contains-duplicates

---
+++ l33tcode/top-interview-questions/arrays/easy/4-contains-duplicate.js
@@ -0,0 +1,24 @@
+// Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+// Example 1:
+
+// Input: nums = [1,2,3,1]
+// Output: true
+// Example 2:
+
+// Input: nums = [1,2,3,4]
+// Output: false
+
+var containsDuplicate = function(nums) {
+  let cache = {};
+  for (let num of nums) {
+    if (cache[num]) return true;
+    else cache[num] = true;
+  }
+  return false;
+};
+
+// Performance
+// Runtime: 128 ms, faster than ~17% of JavaScript online submissions for Best Time to Buy and Sell Stock II.
+
+// Memory Usage: 46.3 MB, less than ~34% of JavaScript online submissions for Best Time to Buy and Sell Stock II
