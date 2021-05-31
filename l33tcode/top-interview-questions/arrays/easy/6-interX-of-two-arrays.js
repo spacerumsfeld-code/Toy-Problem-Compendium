dbdf8939dc9610447750ce96a2c118332ed9c088
Nick Papadakis <nickfin2014@gmail.com>
2021-05-31 14:21:23 -0700

    added solution and perf eval for interX-of-two-arrays

--- l33tcode/top-interview-questions/arrays/easy/6-interX-of-two-arrays.js
+++ l33tcode/top-interview-questions/arrays/easy/6-interX-of-two-arrays.js
@@ -9,3 +9,30 @@
 // Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
 // Output: [4,9]
 // Explanation: [9,4] is also accepted.
+
+var intersect = function(nums1, nums2) {
+  let result = [];
+  let map = new Map();
+  let smaller = nums1.length >= nums2.length ? nums2 : nums1;
+  let larger = smaller === nums2 ? nums1 : nums2;
+
+  for (const num of smaller) {
+      let count = map.get(num) || 0;
+      map.set(num, ++count);
+  }
+
+    for (const num of larger) {
+        let count = map.get(num);
+        if (map.get(num)) {
+            result.push(num);
+            map.set(num, --count);
+        }
+    }
+
+    return result;
+};
+
+// Performance
+// Runtime: 92 ms, faster than ~20% of JavaScript online submissions for Single Number
+
+// Memory Usage: 41.1 MB, less than ~21% of JavaScript online submissions for Single Number.
