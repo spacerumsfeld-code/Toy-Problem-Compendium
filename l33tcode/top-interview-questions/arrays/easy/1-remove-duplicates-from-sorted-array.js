4ef20fbb4c5a99ab4d45d3bfa38fe223546bd619
Nick Papadakis <nickfin2014@gmail.com>
2021-05-31 10:51:20 -0700

    added solution remove-duplicates-from-array

--- l33tcode/top-interview-questions/arrays/easy/1-remove-duplicates-from-sorted-array.js
+++ l33tcode/top-interview-questions/arrays/easy/1-remove-duplicates-from-sorted-array.js
@@ -1,1 +1,19 @@
+// Given a sorted array nums, remove the duplicates in-place such that each element appears only once and returns the new length.
 
+// Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+
+var removeDuplicates = function(nums) {
+  if (nums.length < 2) return nums.length;
+  let p1 = 0;
+  let p2 = 1;
+
+  while (p1 < nums.length) {
+      if (nums[p1] === nums[p2]) nums.splice(p2, 1);
+      else p2++;
+      if (p2 >= nums.length) {
+          p1++;
+          p2 = p1 + 1;
+      }
+  }
+  return nums.length;
+};
