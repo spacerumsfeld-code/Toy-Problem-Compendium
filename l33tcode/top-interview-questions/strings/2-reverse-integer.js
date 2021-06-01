e973c730fd9f2f8353643d5ecede30511e8b4669
Nick Papadakis <nickfin2014@gmail.com>
2021-06-01 13:27:55 -0700

    added reverse integer problem

--- l33tcode/top-interview-questions/strings/2-reverse-integer.js
+++ l33tcode/top-interview-questions/strings/2-reverse-integer.js
@@ -9,3 +9,13 @@
 // Input: s = "loveleetcode"
 // Output: 2
 
+let reverse = 0;
+const limit = 2147483648;
+while(x != 0) {
+    reverse = reverse * 10 + x % 10;
+    x = parseInt(x/10);
+}
+if(reverse < -limit || reverse > limit) {
+    return 0
+}
+return reverse
