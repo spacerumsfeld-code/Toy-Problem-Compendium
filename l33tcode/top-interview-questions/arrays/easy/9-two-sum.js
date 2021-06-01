4c3f19f490f8ae02c538236a7c53566d2a9bef6f
Nick Papadakis <nickfin2014@gmail.com>
2021-05-31 17:26:30 -0700

    added solution for two-sum

--- l33tcode/top-interview-questions/arrays/easy/9-two-sum.js
+++ l33tcode/top-interview-questions/arrays/easy/9-two-sum.js
@@ -1,21 +1,28 @@
-Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 
-You may assume that each input would have exactly one solution, and you may not use the same element twice.
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
 
-You can return the answer in any order.
+// You can return the answer in any order.
 
+// Example 1:
 
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+// Output: Because nums[0] + nums[1] == 9, we return [0, 1].
+// Example 2:
 
-Example 1:
+// Input: nums = [3,2,4], target = 6
+// Output: [1,2]
+// Example 3:
 
-Input: nums = [2,7,11,15], target = 9
-Output: [0,1]
-Output: Because nums[0] + nums[1] == 9, we return [0, 1].
-Example 2:
+// Input: nums = [3,3], target = 6
+// Output: [0,1]
 
-Input: nums = [3,2,4], target = 6
-Output: [1,2]
-Example 3:
+var twoSum = function(nums, target) {
+  let map = new Map();
 
-Input: nums = [3,3], target = 6
-Output: [0,1]
+  for (let i = 0; i < nums.length; i++) {
+      if (map.has(target - nums[i])) return [i, map.get(target - nums[i])];
+      else map.set(nums[i], i);
+  }
+};
