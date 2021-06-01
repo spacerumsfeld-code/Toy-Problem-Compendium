ed0d63ef1aa3784db579ac4f551c92cb3f1725fa
Nick Papadakis <nickfin2014@gmail.com>
2021-06-01 13:21:50 -0700

    added reverse integer problem

---
+++ l33tcode/top-interview-questions/strings/2-reverse-integer.js
@@ -0,0 +1,11 @@
+// Given a string s, return the first non-repeating character in it and return its index. If it does not exist, return -1.
+
+// Example 1:
+
+// Input: s = "leetcode"
+// Output: 0
+// Example 2:
+
+// Input: s = "loveleetcode"
+// Output: 2
+
