365387dd748ba8939351599994f343f5b666d348
Nick Papadakis <nickfin2014@gmail.com>
2021-06-01 13:05:43 -0700

    added string problems section

--- l33tcode/top-interview-questions/introduction.js
+++ l33tcode/top-interview-questions/introduction.js
@@ -1,18 +1,18 @@
-/* Welcome to the Arrays section of the "Top Interview Questions" module of l33tcode. 
-These problems will all pertain to, you guessed it, arrays. 
+/* Welcome to the Arrays section of the "Top Interview Questions" module of l33tcode.
+These problems will all pertain to, you guessed it, arrays.
 
 The point of these exercises is not to simply whip up a solution as fast as possible;
 take as much time as needed during the brainstorming segment of your problem-solving process
 and try to really get into the weeds of what your code is telling the machine to do, including
 the time and space complexity of said code.
 
-The overarching goal is a deeper-seated understanding of the implications of different 
+The overarching goal is a deeper-seated understanding of the implications of different
 instructions to a machine (i.e., code), as well as, of course, sharpening one's interview
 skills.
 
 The problems are delimited by difficulty level. Here are the problems, in order:
 
-Easy
+"Easy" Arrays
 1. Remove Duplicates from Sorted Array
 2. Best Time to Buy and Sell Stock II
 3. Rotate Array
@@ -24,6 +24,17 @@ Easy
 9. Two Sum
 10. Valid Sudoku
 11. Rotate Image
+
+"Easy" Strings
+1. Reverse String
+2. Reverse Integer
+3. First Unique Char in String
+4. Valid Anagram
+5. Valid Palindrome
+6. String to Integer
+7. Implement strStr()
+8. Count and Say
+9. Longest Common Prefix
 
 Medium
 (later)
