9f23883688ca06f3219641866e9c2b54b2783ff0
Nick Papadakis <nickfin2014@gmail.com>
2021-06-01 13:09:24 -0700

    added reverse string solution

--- l33tcode/top-interview-questions/strings/reverse-string.js
+++ l33tcode/top-interview-questions/strings/reverse-string.js
@@ -8,3 +8,11 @@
 
 // Input: s = ["H","a","n","n","a","h"]
 // Output: ["h","a","n","n","a","H"]
+
+// Follow up: Do not allocate extra space for another array. You must do this by modifying the input array in-place with O(1) extra memory.
+
+var reverseString = function(s) {
+  for (let i = 0, j = s.length - 1; i <= j; i++, j--) {
+      [s[i], s[j]] = [s[j], s[i]];
+  }
+};
