5bf2e2fbb04c47acb308eefb030df91a238f10f6
Nick Papadakis <nickfin2014@gmail.com>
2021-06-02 18:33:19 -0700

    added solutuon for merge sorted array

--- l33tcode/top-interview-questions/search-sort/merge-sorted-array.js
+++ l33tcode/top-interview-questions/search-sort/merge-sorted-array.js
@@ -16,3 +16,19 @@
 // Output: [1]
 // Explanation: The arrays we are merging are [1] and [].
 // The result of the merge is [1].
+
+const merge = (nums1, m, nums2, n) => {
+  let left = m - 1
+  let right = n - 1
+
+  for (let i = nums1.length - 1; i >= 0; i--) {
+    if (right < 0) {
+      break;
+    }
+    if (left >= 0 && nums1[left] > nums2[right]) {
+      nums1[i] = nums1[left--];
+    } else {
+      nums1[i] = nums2[right--];
+    }
+  }
+};
