4da840090cfd5069266cce112233c733a8100ae3
Nick Papadakis <nickfin2014@gmail.com>
2021-06-03 18:31:22 -0700

    added naive solution for palindrome list

--- l33tcode/top-interview-questions/linked-lists/easy/5-palindrome-list.js
+++ l33tcode/top-interview-questions/linked-lists/easy/5-palindrome-list.js
@@ -1,17 +1,29 @@
-Given the head of a singly linked list, return true if it is a palindrome.
+// Given the head of a singly linked list, return true if it is a palindrome.
 
-Example 1:
+// Example 1:
 
-Input: head = [1,2,2,1]
-Output: true
-Example 2:
+// Input: head = [1,2,2,1]
+// Output: true
+// Example 2:
 
-Input: head = [1,2]
-Output: false
+// Input: head = [1,2]
+// Output: false
 
-Constraints:
+// Constraints:
 
-The number of nodes in the list is in the range [1, 105].
-0 <= Node.val <= 9
+// The number of nodes in the list is in the range [1, 105].
+// 0 <= Node.val <= 9
 
-Follow up: Could you do it in O(n) time and O(1) space?
+// Follow up: Could you do it in O(n) time and O(1) space?
+
+//naive solution
+var isPalindrome = function(head) {
+  let values = [];
+  let currentNode = head;
+  while (currentNode) {
+      values.push(currentNode.val);
+      currentNode = currentNode.next;
+  }
+  let reverseValues = values.slice().reverse();
+  return values.join('') === reverseValues.join('');
+};
