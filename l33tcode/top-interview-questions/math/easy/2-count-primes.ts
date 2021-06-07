574c07be39c703507087af49ef649f94ba6a01a8
Nick Papadakis <nickfin2014@gmail.com>
2021-06-07 14:28:43 -0700

    added solution for count primes

--- l33tcode/top-interview-questions/math/easy/2-count-primes.ts
+++ l33tcode/top-interview-questions/math/easy/2-count-primes.ts
@@ -16,3 +16,21 @@
 
 //  Constraints:
 // 0 <= n <= 5 * 106
+
+var countPrimes = function(n) {
+  const arr = new Array(n);
+  arr.fill(false);
+  let count = 0;
+  if (n > 2) count = 1;
+  for (let i = 3; i < n; i += 2) {
+    if (arr[i] === false) {
+      count++;
+      for (let j = 3; i * j < n; j += 2) {
+        arr[i * j] = true;
+      }
+    }
+  }
+
+  return count;
+};
+
