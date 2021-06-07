7a500e91d92ad4c60491fbeef40b419032aafc38
Nick Papadakis <nickfin2014@gmail.com>
2021-06-07 15:38:38 -0700

    added solution for valid parentheses

--- l33tcode/top-interview-questions/misc/7-valid-parentheses.ts
+++ l33tcode/top-interview-questions/misc/7-valid-parentheses.ts
@@ -29,3 +29,16 @@
 // Constraints:
 // 1 <= s.length <= 104
 // s consists of parentheses only '()[]{}'.
+
+function isValid(s: string): boolean {
+  let stack: string[] = [];
+
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === ')' && stack[stack.length - 1] === '(') stack.pop();
+    else if (s[i] === ']' && stack[stack.length - 1] === '[') stack.pop();
+    else if (s[i] === '}' && stack[stack.length - 1] === '{') stack.pop();
+    else stack.push(s[i]);
+  }
+
+  return !stack.length;
+};
