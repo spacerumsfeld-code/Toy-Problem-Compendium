fb38f011044d4c0d2275285cea7719974a105b11
Nick Papadakis <nickfin2014@gmail.com>
2021-06-25 15:13:43 -0700

    completed summary file for Arrays-101

---
+++ arrays-101/summary.js
@@ -0,0 +1,33 @@
+/* Leetcode "Arrays 101" collection
+note: there may be some overlap with other collections, but that is okay.
+It's another rep and a chance to understand the problem more deeply.
+
+Problems
+
+Introduction
+1. Max Consecutive Ones
+2. Squares of a Sorted Array
+3. Find Numbers with Even Number of Digits
+
+Inserting Items into Array
+4. Duplicate Zeroes
+5. Merge Sorted Array
+
+Deleting Items from Array
+6. Remove Element
+7. Remove Duplicates from Sorted Array
+
+Searching in an Array
+8. Valid Mountain Array
+9. Check if N and its Double Exists
+
+In-place Operations
+10. Sort Array by Parity
+11. Replace Elements with Greatest Element on Right Side
+12. Move Zeroes
+
+Extra problems
+13. Height Checker
+14. Third Maximum Number
+15. Max Consecutive Ones II
+16. Find All Numbers Disappeared
