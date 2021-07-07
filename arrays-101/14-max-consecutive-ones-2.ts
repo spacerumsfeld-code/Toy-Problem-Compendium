2943b21babd5bd2167b9b8fcad5a8e1bc5bf7ea3
Nick Papadakis <nickfin2014@gmail.com>
2021-07-07 15:11:50 -0700

    added solution for max consec 1s 2

--- arrays-101/14-max-consecutive-ones-2.ts
+++ arrays-101/14-max-consecutive-ones-2.ts
@@ -14,3 +14,27 @@
 
 
 // Follow up: What if the input numbers come in one by one as an infinite stream? In other words, you can't store all numbers coming from the stream as it's too large to hold in memory. Could you solve it efficiently?
+
+var findMaxConsecutiveOnes = function(nums) {
+  let currentStreak = 0;
+  let maxStreak = 0;
+  let indexOfZero;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === 0) {
+      if (indexOfZero !== undefined) {
+        let difference = i - indexOfZero;
+        indexOfZero = i;
+        currentStreak = difference;
+      } else {
+        currentStreak++;
+        indexOfZero = i;
+      }
+    } else {
+      currentStreak++;
+    }
+    maxStreak = Math.max(currentStreak, maxStreak);
+  }
+
+  return maxStreak;
+};
