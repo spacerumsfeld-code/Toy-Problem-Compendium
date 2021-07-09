83617d15f982d1fa9846c9ee70ad9b28a3e0da72
Nick Papadakis <nickfin2014@gmail.com>
2021-07-09 15:06:24 -0700

    added solution for interX of two arrays

--- problem collections/hashTables-101/5-interX-of-two-arrays.ts
+++ problem collections/hashTables-101/5-interX-of-two-arrays.ts
@@ -12,3 +12,15 @@
 // Constraints:
 // 1 <= nums1.length, nums2.length <= 1000
 // 0 <= nums1[i], nums2[i] <= 1000
+
+var intersection = function(nums1, nums2) {
+  let set1 = new Set(nums1);
+  let set2 = new Set(nums2);
+  let intersection = new Set();
+
+  for (const num of set1) {
+    if (set2.has(num)) intersection.add(num);
+  }
+
+  return [...intersection];
+};
