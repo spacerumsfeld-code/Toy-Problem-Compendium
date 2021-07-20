6958f75f1c7c80f4e539b0dc9a70cb2f20f507c4
Nick Papadakis <nickfin2014@gmail.com>
2021-07-20 13:43:56 -0700

    added solution to binarySearch

--- problem collections/binarySearch/1-binarySearch.ts
+++ problem collections/binarySearch/1-binarySearch.ts
@@ -17,3 +17,18 @@
 // -104 < nums[i], target < 104
 // All the integers in nums are unique.
 // nums is sorted in ascending order.
+
+var search = function(nums: number[], target: number): number {
+  let start = 0;
+  let end = nums.length - 1;
+  let mid = Math.floor((start + end) / 2);
+
+  while (start < end && nums[mid] !== target) {
+    if (nums[mid] > target) end = mid - 1;
+    else start = mid + 1;
+    mid = Math.floor((start + end) / 2);
+  }
+
+  if (nums[mid] === target) return mid;
+  return -1;
+};
