79b87c0b75171787ec2594882b5dc1f4ce9148cd
Nick Papadakis <nickfin2014@gmail.com>
2021-08-11 12:48:10 -0700

    rebaseTestingSomeMore

--- problem collections/arrays-and-strings/1-pivot-index.ts
+++ problem collections/arrays-and-strings/1-pivot-index.ts
@@ -7,7 +7,7 @@
 // Return the leftmost pivot index. If no such index exists, return -1.
 
 // Example 1:
-// Input: nums = [0,1,2,3,45,6,6,7,6,76,7,68,68,]
+// Input: nums = [0,2,4,6,8,20]
 //hallelujah
 // Output: 3
 // Explanation:
